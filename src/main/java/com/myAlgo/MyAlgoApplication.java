8ef656353c8a345f4b7c920d5f8673673734a666
jki12 <sjak0227@naver.com>
2023-04-06 17:09:26 +0900

    init

---
+++ src/main/java/com/myAlgo/MyAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.myAlgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAlgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myAlgo/MyAlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myAlgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAlgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
